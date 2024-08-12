fc2a9a1ca3c406046d966c6b8a0c135bedf821c1
Dibyajyoti Mohanty <dibyajyotimohanty8812@gmail.com>
2024-08-12 20:36:41 +0530

    Calculator web app created

---
+++ script.js
@@ -0,0 +1,42 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+let body=document.querySelectorAll('body');
+let theme= document.querySelector('.theme');
+let sun=document.querySelector('.icon-sun');
+let moon=document.querySelector('.icon-moon');
+theme.onclick = function(){
+    document.body.classList.toggle("light");
+    if(document.body.classList.contains("light"))
+    {
+        sun.disabled=true;
+        moon.disabled=false;
+    }
+    else{
+        sun.disabled=false;
+        moon.disabled=true;
+    }
+}
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string = string + e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
